f5b6eeb226f8ba4f7314dbec67ca7e5a93797634
Estudiante <Estudiante@FI454402-09.unal.loc>
2016-08-09 15:18:08 -0500

    Ejercicio inicial

---
+++ src/Ejercicio2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Ejercicio2 {
+    public static void main (string[] args) {
+        int x=0;
+        
+    }
+}
